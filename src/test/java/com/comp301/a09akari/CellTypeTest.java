81db919f1412ab8b3c2ce00081cca572a0cba699
Javier lorenzo <92158938+alu0101321257@users.noreply.github.com>
2023-12-19 19:55:21 +0000

    Update CellTypeTest.java (doc)

--- src/test/java/com/comp301/a09akari/CellTypeTest.java
+++ src/test/java/com/comp301/a09akari/CellTypeTest.java
@@ -1,3 +1,7 @@
+/**
+ * @file CellTypeTest.java
+ * @brief This file contains the unit tests for the CellType enumeration.
+ */
 package com.comp301.a09akari;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -6,19 +10,33 @@ import com.comp301.a09akari.controller.*;
 import com.comp301.a09akari.model.*;
 import com.comp301.a09akari.view.*;
 import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * @brief This class contains unit tests for the CellType enumeration.
+ */
 public class CellTypeTest {
-
+  /**
+   * @brief Test for the CLUE CellType.
+   * @details This test checks if the valueOf("CLUE") method of the CellType enumeration returns
+   * the expected CellType value.
+   */
   @Test
   public void testClueCellType() {
     assertEquals(CellType.CLUE, CellType.valueOf("CLUE"));
   }
-
+  /**
+   * @brief Test for the CORRIDOR CellType.
+   * @details This test checks if the valueOf("CORRIDOR") method of the CellType enumeration returns
+   * the expected CellType value.
+   */
   @Test
   public void testCorridorCellType() {
     assertEquals(CellType.CORRIDOR, CellType.valueOf("CORRIDOR"));
   }
-
+   /**
+   * @brief Test for the WALL CellType.
+   * @details This test checks if the valueOf("WALL") method of the CellType enumeration returns
+   * the expected CellType value.
+   */
   @Test
   public void testWallCellType() {
     assertEquals(CellType.WALL, CellType.valueOf("WALL"));
