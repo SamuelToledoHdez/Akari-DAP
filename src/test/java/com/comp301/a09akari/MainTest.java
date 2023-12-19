3ee24c582dcc603c1cf0bdc103b8c5011af3e9bb
Javier lorenzo <92158938+alu0101321257@users.noreply.github.com>
2023-12-19 19:57:51 +0000

    Update MainTest.java (doc)

--- src/test/java/com/comp301/a09akari/MainTest.java
+++ src/test/java/com/comp301/a09akari/MainTest.java
@@ -1,12 +1,22 @@
+/**
+ * @file MainTest.java
+ * @brief This file contains unit tests for the Main class.
+ */
 package com.comp301.a09akari;
 import com.comp301.a09akari.view.AppLauncher;
 import javafx.application.Application;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
-
+/**
+ * @brief This class contains unit tests for the Main class.
+ */
 public class MainTest {
-
+    /**
+     * @brief Test to ensure that launching the application does not result in exceptions.
+     * @details This test attempts to launch the application using the Main class and ensures
+     * that no exceptions are thrown during the process.
+     */
     @Test
     public void testMainLaunchesApp() {
         // Intenta lanzar la aplicación y asegúrate de que no se produzcan excepciones
@@ -20,7 +30,10 @@ public class MainTest {
         // Si no se lanzan excepciones, la prueba pasa
         assert true;
     }
-
+    /**
+     * @brief Test to ensure that the AppLauncher class is present and accessible.
+     * @details This test checks if the AppLauncher class is present and can be accessed.
+     */
     @Test
     public void testAppLauncherIsPresent() {
         // Asegúrate de que la clase AppLauncher esté presente y accesible
